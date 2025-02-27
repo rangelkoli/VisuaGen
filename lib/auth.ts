745460aed22ee50fc7fa0821f1cf66f7e364bc34
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-02-27 01:04:33 -0500

    fix: update redirect URL for Google sign-in to remove trailing slash

--- lib/auth.ts
+++ lib/auth.ts
@@ -33,7 +33,7 @@ export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_URL}/`,
+      redirectTo: `${process.env.NEXT_PUBLIC_URL}`,
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
